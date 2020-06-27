566aa8c5f5d869e59c92c04bb5619086c95830a0
Susanta <inbox.susanta@gmail.com>
2020-06-27 13:50:56 +0530

    Adding data to Firestore

--- index.js
+++ index.js
@@ -23,3 +23,24 @@ btns.forEach((btn) => {
     formAct.textContent = activity;
   });
 });
+
+// form submit
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const distance = parseInt(input.value);
+  if (distance) {
+    db.collection('activities')
+      .add({
+        distance,
+        activity,
+        date: new Date().toString(),
+      })
+      .then(() => {
+        error.textContent = '';
+        input.value = '';
+      });
+  } else {
+    error.textContent = 'Please enter a valid distance';
+  }
+});
