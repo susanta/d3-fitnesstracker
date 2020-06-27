73cc872143ac55671aa5b446c03ec7394df6d662
Susanta <inbox.susanta@gmail.com>
2020-06-27 17:50:14 +0530

    Line path generator

--- graph.js
+++ graph.js
@@ -26,14 +26,39 @@ const xAxisGroup = graph
 
 const yAxisGroup = graph.append('g').attr('class', 'y-axis');
 
+// d3 line path generator
+const line = d3
+  .line()
+  .x(function (d) {
+    return x(new Date(d.date));
+  })
+  .y(function (d) {
+    return y(d.distance);
+  });
+
+//   line path element
+const path = graph.append('path');
+
 const update = (data) => {
   //   console.log(data);
 
+  //   filter data
   data = data.filter((item) => item.activity == activity);
+
+  //   sort data based on date objects
+  data.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   // set scale domains
   x.domain(d3.extent(data, (d) => new Date(d.date)));
   y.domain([0, d3.max(data, (d) => d.distance)]);
+
+  //  update path data
+  path
+    .data([data])
+    .attr('fill', 'none')
+    .attr('stroke', '#00bfa5')
+    .attr('stroke-width', 2)
+    .attr('d', line);
 
   //   create circles for objects
   const circles = graph.selectAll('circle').data(data);
